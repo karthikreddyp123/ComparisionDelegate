6ae474c760b430311b964cd7501bbf7b897acffb
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-01-31 14:46:45 +0530

    Add project files.

---
+++ ComparisionDelegate/ComparisionDelegate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisionDelegate
+{
+    class ComparisionDelegate
+    {
+        static void Main(string[] args)
+        {
+            //Array of Objects
+            Student[] arrStudent = new Student[] { new Student() { Name = "Karthik" },
+            new Student() { Name = "Rahul" },
+            new Student() { Name = "Vivek" },
+            new Student() { Name = "Aditya" }};
+
+            Console.WriteLine("Before Sorting:");
+            foreach (Student i in arrStudent)
+            {
+                Console.WriteLine(i.Name);
+            }
+            //Sort using delagate
+            Array.Sort(arrStudent, delegate (Student s1, Student s2) { return s1.Name.CompareTo(s2.Name); });
+            Console.WriteLine("After Sorting:");
+            foreach (Student i in arrStudent)
+            {
+                Console.WriteLine(i.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class Student : IComparable
+    {
+        public string Name { get; set; }
+        //Defining CompareTo method
+        public int CompareTo(object obj)
+        {
+            Student stuObj = obj as Student;
+            if (stuObj != null)
+            {
+                return this.Name.CompareTo(stuObj.Name);
+            }
+            return -1;
+        }
+    }
+}
